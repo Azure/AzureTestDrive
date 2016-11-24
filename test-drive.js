039789d07845c04d9202749c6e3e893bc2ebf0cf
Sergei Popov <serpop@microsoft.com>
2016-11-23 20:57:10 -0800

    Add 'validate template' command.

---
+++ test-drive.js
@@ -0,0 +1,16 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+
+"use strict";
+
+var args = process.argv.slice(2);
+
+if (args[0] === "validate") {
+    require("./lib/validate-command.js").run({
+        templateFileName: args[1]
+    });
+}
+else {
+    throw new Error(`Unknown command: ${args[0]}`);
+}
