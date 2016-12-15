a5b608d50fd81e6b968dcf49d1f1c76037e7ecea
Sergei Popov <serpop@microsoft.com>
2016-12-15 12:10:15 -0800

    Include NuDet download into sample web app build script.

--- samples/contoso_webapp/src/build.cmd
+++ samples/contoso_webapp/src/build.cmd
@@ -1,4 +1,6 @@
 call "%VS140COMNTOOLS%\vsvars32.bat" || goto :EOF
-cmd /C nuget restore ContosoWebApp.sln || goto :EOF
+powershell -NoProfile -NonInteractive "if (-not (Test-Path temp)) { md temp | Out-Null }"
+powershell -NoProfile -NonInteractive "if (-not (Test-Path temp\nuget.exe)) { Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile temp\nuget.exe }"
+temp\nuget restore ContosoWebApp.sln || goto :EOF
 MSBuild /t:Build /p:Configuration=Release /p:DeployOnBuild=true /p:PublishProfile=Properties\PublishProfiles\DefaultProfile.pubxml || goto :EOF
 powershell -NoProfile -NonInteractive "Write-Host 'Success' -ForegroundColor Green"
